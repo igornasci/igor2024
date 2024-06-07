36426b45388f3475dd29ea3f6e97112a10028050
igornasci <132663504+igornasci@users.noreply.github.com>
2024-06-07 08:34:01 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-const botoes = document.querySelectorAll('.botao');
+document.querySelectorAll('.botao').forEach(btn => {
+    btn.addEventListener('click', function() {
 
+        document.querySelectorAll('.aba-conteudo').forEach(tab => {
+            tab.classList.remove('ativo');
+        // document.querySelectorAll('.botao').forEach(tab =>{
+        //         tab.classList.remove('ativo');
+        });
+        const index = Array.from(document.querySelectorAll('.botao')).concat
+        document.querySelectorAll('.aba-conteudo')[index].classList.add
+        document.querySelectorAll('.botaoa')[index].classList.add('ativo');
+    });
+});
+
+const botoes = document.querySelectorAll('.botao');
 for(let i=0;i <botoes.length;i++){
-    botoes[i].onclick=function() {
-        for(let j=0;j<botoes.length;j++)  {
-botoes[j].classList.remove('ativo');
+    botoes[i].onclick = function() {
+        for(let j=0;j<botoes.length;j++) {
+            botoes[j].classList.remove('ativo'); }
+        botoes[i].classList.add('ativo');}
         }
-        botoes[i].classList.add('ativo');
-    }
-}
